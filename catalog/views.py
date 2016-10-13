d7a8f612311d9f016c915fe2a3f71264061a6a9b
Mohaiminul Hasan <hasan.mohaiminul@gmail.com>
2016-10-13 16:07:30 +0600

    4.07

--- catalog/views.py
+++ catalog/views.py
@@ -78,8 +78,8 @@ def auto(request):
         except IntegrityError:
             dosage = None
 
-        try:
+        slug = slugify(datum[0])
+        prod = Product.objects.get(slug=slug)
+        if not prod.exists():
             Product.objects.create(name=datum[0], generic=datum[1], manufacturer=manufacturer, price=datum[3], is_active=datum[4], unit=datum[5], dosage=dosage, category=category, subcategory=subcategory)
-        except IntegrityError:
-            return HttpResponse("Integrity Error!")
     return HttpResponseRedirect('/')
