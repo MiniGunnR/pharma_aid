34274ceecab1594f94b653477bbae435bcf822eb
Mohaiminul Hasan <hasan.mohaiminul@gmail.com>
2016-10-13 15:45:53 +0600

    add integrity error exception to product save

--- catalog/views.py
+++ catalog/views.py
@@ -78,5 +78,8 @@ def auto(request):
         except IntegrityError:
             dosage = None
 
-        prod = Product.objects.create(name=datum[0], generic=datum[1], manufacturer=manufacturer, price=datum[3], is_active=datum[4], unit=datum[5], dosage=dosage, category=category, subcategory=subcategory)
+        try:
+            prod = Product.objects.create(name=datum[0], generic=datum[1], manufacturer=manufacturer, price=datum[3], is_active=datum[4], unit=datum[5], dosage=dosage, category=category, subcategory=subcategory)
+        except IntegrityError:
+            pass
     return HttpResponseRedirect('/')
