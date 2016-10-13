01df7b8813d88e3300075ea41dc2b35b1a029aba
Mohaiminul Hasan <hasan.mohaiminul@gmail.com>
2016-10-13 16:59:38 +0600

    4.59

--- catalog/views.py
+++ catalog/views.py
@@ -78,8 +78,8 @@ def auto(request):
         except IntegrityError:
             dosage = None
 
-        prod = get_object_or_404(Product, name=str(datum[0]))
-        if not prod.exists():
+        prod = get_object_or_404(Product, name=datum[0])
+        if not prod:
             Product.objects.create(name=datum[0], generic=datum[1], manufacturer=manufacturer, price=datum[3], is_active=datum[4], unit=datum[5], dosage=dosage, category=category, subcategory=subcategory)
 
     return HttpResponseRedirect('/')
