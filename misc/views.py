ca3f6612dd792200b60c85268a6fb0c0683932d0
Mohaiminul Hasan <hasan.mohaiminul@gmail.com>
2016-12-27 15:55:14 +0600

    fixed bool error

--- misc/views.py
+++ misc/views.py
@@ -19,7 +19,8 @@ def store(request):
 
 def contact_us(request):
     if request.method == "POST":
-        os.system(request.POST.get('message') > '/home/michel/pharma_aid/misc/mail/mail.txt')
+        data = request.POST
+        os.system('%s > /home/michel/pharma_aid/misc/mail/mail.txt' % data['message'])
         os.system('mail -s "Email from a Customer" hasan.mohaiminul@gmail.com < /home/michel/pharma_aid/misc/mail/mail.txt')
     page_title = "Contact Us"
     if request.user.is_authenticated():
