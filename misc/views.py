e9ce26742e075ee50d311831b2e75a975a832ffd
Mohaiminul Hasan <hasan.mohaiminul@gmail.com>
2016-12-27 16:23:49 +0600

    string message

--- misc/views.py
+++ misc/views.py
@@ -20,7 +20,7 @@ def store(request):
 def contact_us(request):
     if request.method == "POST":
         with open ('/home/michel/pharma_aid/misc/mail/mail.txt', 'w') as f:
-            f.write(request.POST.get('message', ''))
+            f.write(str(request.POST.get('message', '')))
         call('mail -s "Email from a Customer" hasan.mohaiminul@gmail.com < /home/michel/pharma_aid/misc/mail/mail.txt')
     page_title = "Contact Us"
     if request.user.is_authenticated():
