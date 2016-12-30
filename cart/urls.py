1d8aa79cb8bd8ede9690b655d033246a1e191065
Mohaiminul Hasan <hasan.mohaiminul@gmail.com>
2016-12-30 13:38:22 +0600

    add to monthly working

--- cart/urls.py
+++ cart/urls.py
@@ -3,10 +3,11 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^add/to/monthly/(?P<slug>.*)/$', views.add_to_monthly, name='add-to-monthly'),
+
     url(r'^add/(?P<slug>.*)/$', views.add_to_cart, name='add-to-cart'),
     url(r'^remove/(?P<slug>.*)/$', views.remove_from_cart, name='remove-from-cart'),
     url(r'^delete/(?P<slug>.*)/', views.delete_from_cart, name='delete-from-cart'),
 
     url(r'^monthly/order/$', views.monthly_order, name='monthly-order'),
-    url(r'^add/to/monthly/(?P<slug>.*)/$', views.add_to_monthly, name='add-to-monthly'),
 ]
