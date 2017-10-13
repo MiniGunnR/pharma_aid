9679fbbe0e5b1162273d7fe692e02a7e97554bbc
MiniGunnR <hasan.mohaiminul@gmail.com>
2017-10-13 22:14:45 +0600

    new is_active

--- catalog/views.py
+++ catalog/views.py
@@ -84,13 +84,8 @@ def auto(request):
         else:
             price = datum[3]
 
-        if datum[11] is None:
-            is_active = 0
-        else:
-            is_active = datum[11]
-
         try:
-            Product.objects.create(name=datum[0], generic=datum[1], manufacturer=manufacturer, price=price, is_active=is_active, unit=datum[5], dosage=dosage, category=category, subcategory=subcategory)
+            Product.objects.create(name=datum[0], generic=datum[1], manufacturer=manufacturer, price=price, is_active=datum[11], unit=datum[5], dosage=dosage, category=category, subcategory=subcategory)
         except IntegrityError as e:
             pass
 
