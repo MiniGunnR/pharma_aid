57b7701a33bced65f60411f1f2efc2bdfb4a4fa9
MiniGunnR <hasan.mohaiminul@gmail.com>
2019-04-25 18:12:09 +0600

    debug false

--- pharma_aid/settings.py
+++ pharma_aid/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ldt3jvaks6$3c^(!=#bmgmb*_qf^6$lul%ot5cfed%1o4f%z0i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
