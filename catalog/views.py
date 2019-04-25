5c4119501680734f61d92ad316aab7c4451b9fc7
MiniGunnR <hasan.mohaiminul@gmail.com>
2019-04-25 09:47:26 +0600

    sldkf

--- catalog/views.py
+++ catalog/views.py
@@ -83,7 +83,7 @@ def auto(request):
         if datum[3] == "":
             price = 0.0
         else:
-            price = float(datum[3])
+            price = float(int(datum[3]))
 
         try:
             Product.objects.create(name=datum[0], generic=datum[1], manufacturer=manufacturer, price=price, is_active=datum[11], unit=datum[5], dosage=dosage, category=category, subcategory=subcategory)
