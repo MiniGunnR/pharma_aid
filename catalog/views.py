0eb61650a3180620cd87f8c7399c45cc6102d5ad
MiniGunnR <hasan.mohaiminul@gmail.com>
2019-04-25 09:22:00 +0600

    convert price to float

--- catalog/views.py
+++ catalog/views.py
@@ -80,9 +80,9 @@ def auto(request):
             dosage = None
 
         if datum[3] == "":
-            price = 0
+            price = 0.0
         else:
-            price = datum[3]
+            price = float(datum[3])
 
         try:
             Product.objects.create(name=datum[0], generic=datum[1], manufacturer=manufacturer, price=price, is_active=datum[11], unit=datum[5], dosage=dosage, category=category, subcategory=subcategory)
