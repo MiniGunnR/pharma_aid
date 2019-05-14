e5824c95c97d8a310fa1707241f9c670b59bb3a8
MiniGunnR <hasan.mohaiminul@gmail.com>
2019-05-14 13:58:27 -0400

    changed static url

--- pharma_aid/settings.py
+++ pharma_aid/settings.py
@@ -124,7 +124,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/' if (sys.argv[1] == 'runserver') else '/staticfiles/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
